5111518624514bc193fbb8e7f7a1c60203b5cc93
laptop <laptop@laptop.com>
2025-07-13 19:22:34 +0700

    hl built, sdl built

--- android/run.py
+++ android/run.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shutil
 
 ANDROID_NDK = os.environ["ANDROID_NDK"]
 
@@ -17,36 +18,58 @@ def run_command(command):
 
     return process.returncode
 
-print("----> cleaning build dir");
-exit_code = run_command(command = ["rm", "-rf", "build"])
+def cleaning():
+    print("----> cleaning build dir");
+    if not os.path.isdir("build"):
+        return
+    
+    for entry in os.listdir("build"):
+        if entry == "_deps":
+            continue
+        else:
+            path = os.path.join("build", entry)
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
 
-if(exit_code != 0):
-    print("Exit code:", exit_code)
-    exit(1)
+def configuring():
+    print("----> configuring");
+    exit_code = run_command(command = [
+            "cmake",
+            "-B",
+            "build",
+            "-G",
+            "Ninja",
+            "-DANDROID_ABI=arm64-v8a",
+            "-DANDROID_PLATFORM=android-21",
+            f"-DANDROID_NDK={ANDROID_NDK}",
+            f"-DCMAKE_TOOLCHAIN_FILE={ANDROID_NDK}/build/cmake/android.toolchain.cmake"
+        ])
 
-print("----> configuring");
-exit_code = run_command(command = [
-        "cmake",
-        "-B",
-        "build",
-        "-G",
-        "Ninja",
-        "-DANDROID_ABI=arm64-v8a",
-        "-DANDROID_PLATFORM=android-21",
-        f"-DANDROID_NDK={ANDROID_NDK}",
-        f"-DCMAKE_TOOLCHAIN_FILE={ANDROID_NDK}/build/cmake/android.toolchain.cmake"
-    ])
+    if(exit_code != 0):
+        print("Exit code:", exit_code)
+        exit(1)
 
-if(exit_code != 0):
-    print("Exit code:", exit_code)
-    exit(1)
+def building():
+    print("----> building...");
+    exit_code = run_command(command = ["cmake", "--build", "build"])
 
-print("----> building...");
-exit_code = run_command(command = ["cmake", "--build", "build"])
+    if(exit_code != 0):
+        print("Exit code:", exit_code)
+        exit(1)
 
-if(exit_code != 0):
-    print("Exit code:", exit_code)
-    exit(1)
+def packaging():
+    print("----> packaging...[libs and includes will be in the directory: package-export]");
+    exit_code = run_command(command = ["cmake", "--install", "build", "--prefix", "package-export"])
 
+    if(exit_code != 0):
+        print("Exit code:", exit_code)
+        exit(1)
+
+# cleaning()
+configuring()
+building()
+packaging()
 
 print("------> DONE")
